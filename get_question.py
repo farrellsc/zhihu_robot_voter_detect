6b2058b58dd91256d43a126d0aadbd194a70acc9
Zhuang Zhou <farrellsc@163.com>
2016-02-08 23:54:57 +0800

    Update get_question.py

--- get_question.py
+++ get_question.py
@@ -13,8 +13,18 @@ class Question:
             'Text':None,
             'Answernum':None,
             'Followernum':0,
+            'Publish':None,
+            'Updated':'Never Updated',
+            'Reviewed':0,
             'Answers': {}
             }
+    def printq(self):
+        print 'Text',self.data['Text']
+        print 'Answernum',self.data['Answernum']
+        print 'Followernum',self.data['Followernum']
+        print 'Publish',self.data['Publish']
+        print 'Updated',self.data['Updated']
+        print 'Reviewed',self.data['Reviewed']
 
 def process_question_page(driver):
     for i in range(1,200):
@@ -30,7 +40,13 @@ def get_question(driver):
     question=Question()
     question.data['Text']=driver.find_element_by_css_selector("h2[class=\"zm-item-title zm-editable-content\"]").text[:-2]
     question.data['Answernum']=driver.find_element_by_css_selector("h3[id=\"zh-question-answer-num\"]").text[:-3]
-    question.data['Followernum']=driver.find_element_by_css_selector('a[href=\"/question/22722259/followers\"]').text
+    question.data['Followernum']=driver.find_element_by_css_selector('a[href=\"/question/40175228/followers\"]').text
+    gray=driver.find_elements_by_css_selector('div[class=\"zg-gray-normal\"]')
+    #line=question.data['Reviewed']=gray[2].text
+    #line=line.split('，')[0]
+    #question.data['Reviewed']=line.split(' ')[1]   review not done
+    question.data['Updated']=driver.find_element_by_class_name('time').text
+    question.data['Publish']=driver.find_element_by_css_selector('a[class=\"answer-date-link meta-item\"]').text[4:]
     return question
 
 
